909d742463802a70760f93cfece39f8cb86d9975
Aaron Jackson <yolanother@gmail.com>
2020-03-16 20:37:48 -0700

    Add simple MM stamper tool

---
+++ Generators/MapMagicStamperGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using MapMagic;
+using UnityEditor;
+using UnityEngine;
+using static MapMagic.Generator;
+
+namespace com.doubtech.mapmagic.generators {
+	[System.Serializable]
+	[GeneratorMenu(menu = "Map", name = "Stamper", disengageable = true)]
+	public class MapMagicStamperGenerator : TextureInput {
+        private MapMagicStamper stamper;
+
+        public override void OnGUI(GeneratorsAsset gens) {
+            Transform mm = MapMagic.MapMagic.instance.gameObject.transform;
+            stamper = mm.GetComponentInChildren<MapMagicStamper>();
+            
+
+            base.OnGUI(gens);
+            if ((null == stamper || null != stamper && stamper.Generator != this) && layout.Button("Show Stamper")) {
+                if (null == stamper || stamper.Generator != this) {
+                    if (null != stamper) {
+                        GameObject.DestroyImmediate(stamper.gameObject);
+                    }
+                    GameObject go = new GameObject("Stamper");
+                    go.transform.parent = mm;
+                    stamper = go.AddComponent<MapMagicStamper>();
+                    stamper.transform.localScale = new Vector3(scale, intensity, scale);
+                    stamper.Offset = new Vector3(offset.x, 0, offset.y);
+                    stamper.Generator = this;
+                    Selection.activeObject = stamper;
+                }
+            } else if (null != stamper && stamper.Generator == this && layout.Button("Hide Stamper")) {
+                GameObject.DestroyImmediate(stamper.gameObject);
+            }
+        }
+    }
+}
---
+++ Utilities/MapMagicStamper.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using com.doubtech.mapmagic.generators;
+using UnityEngine;
+
+
+namespace com.doubtech.mapmagic.generators {
+    [ExecuteInEditMode]
+    public class MapMagicStamper : MonoBehaviour {
+        public MapMagicStamperGenerator Generator { get; internal set; }
+
+        public Vector3 Offset {
+            get {
+                return transform.position - Vector3.one * MapMagic.MapMagic.instance.terrainSize * transform.localScale.x / 2.0f;
+            } set {
+                transform.position = value + Vector3.one * MapMagic.MapMagic.instance.terrainSize * transform.localScale.x / 2.0f;
+            }
+        }
+
+        private void OnEnable() {
+            MapMagic.MapMagic.OnGenerateCompleted += MapMagic_OnGenerateCompleted;
+        }
+
+        private void MapMagic_OnGenerateCompleted(Terrain terrain) {
+
+        }
+
+        void Update() {
+            Vector2 offset = new Vector2(Offset.x, Offset.z);
+            float scale = transform.localScale.x;
+            float intensity = transform.localScale.y;
+
+            // Adjust transform to valid stamper coords
+            transform.localScale = new Vector3(
+                transform.localScale.x,
+                Mathf.Clamp(transform.localScale.y, 0, 1),
+                transform.localScale.x);
+
+            if (transform.hasChanged) {
+                Generator.offset = offset;
+                Generator.scale = scale;
+                Generator.intensity = intensity;
+                MapMagic.MapMagic.instance.ClearResults();
+                MapMagic.MapMagic.instance.Generate(true);
+            }
+        }
+
+        private void OnDrawGizmos() {
+            MapMagic.MapMagic mm = MapMagic.MapMagic.instance;
+            float size = mm.terrainSize;
+            Vector3 cubeSize = new Vector3(
+                    size * transform.localScale.x,
+                    mm.terrainHeight * transform.localScale.y,
+                    size * transform.localScale.z
+                    );
+            Vector3 position = new Vector3(
+                Offset.x + cubeSize.x / 2.0f,
+                MapMagic.MapMagic.instance.transform.position.y + mm.terrainHeight / 2.0f * transform.localScale.y,
+                Offset.z + cubeSize.z / 2.0f
+                );
+            Gizmos.DrawWireCube(position, cubeSize);
+        }
+    }
+}
